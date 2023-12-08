d3bc8450a94471d049ceeac979374d72bdf9309c
Prajwal Bharadwaj <prajwal17@iiserb.ac.in>
2023-12-08 20:39:18 +0000

    Updated the cluster visualisation code

--- visualise_clusters.r
+++ visualise_clusters.r
@@ -8,7 +8,7 @@ library(ggtreeExtra)
 
 # Where are the input files stored?
 TREEPATH <- "input/tree2.nwk"
-CLUSPATH <- "temp/cluster_output_Sep20_875/clusters_Sep20.txt"
+CLUSPATH <- "input/clusters.txt"
 fortytwo <- c(12, 66, 74, 79, 96, 111, 141, 150, 159, 873, 880, 906, 912, 924, 929, 934, 936, 945, 949, 952)
 clusters <- read.csv(CLUSPATH, sep=' ', header=FALSE)
 colnames(clusters) <- c('cluster', 'genome', 'start', 'end', 'color', 'seq')
@@ -20,38 +20,72 @@ clusters$genome <- gsub('chl', '', clusters$genome)
 # CAUSE ERRORS SINCE THE REPINS DATASET HAS IT BUT THE TREE DOES NOT
 clusters <- clusters[clusters$genome != 'StFRB508', ]
 clusters <- clusters[clusters$genome != 'O6', ]
+clusters <- clusters[clusters$genome != 'PCL1606', ]
 clusters$cluster <- as.factor(clusters$cluster)
+
+# Testing cases where there are multiple REPINs of the same type in a cluster
+# clusters[nrow(clusters) + 1,] = c('952', 'TAMOak81', 'type2')
+# clusters[nrow(clusters) + 1,] = c('952', '189', 'type2')
+
+
 # Input data
 tree <- read.tree(TREEPATH)
 rayttree <- read.csv('./input/rayts.csv', sep=',')
 
 # ---------------------------------------------------------------------------------------------------------
 # ---------------------------------------------------------------------------------------------------------
-xv <- 50
+xv <- 55
 plot1 <- ggtree(tree, branch.length = 'none')  +
   geom_tiplab(align=TRUE, linesize=.5) + xlim(0, xv) + 
   geom_fruit(data=rayttree,geom=geom_tile,
              mapping = aes(y=genome, x=Color, fill=rayt),
-             offset=0.2,pwidth=0.05) +
+             pwidth=0.1, offset=0.25) +
   geom_treescale()
 
+#fortytwo=c(924, 936, 952)
+numClus=length(fortytwo)
 for (cn in fortytwo){
-  cname <- as.character(cn)
+  cname <- as.character(cn) #141 DOES NOT WORK
   dclus <- clusters[clusters$cluster == cname, ]
+  
+  genomelist <- unique(dclus$genome)
+  for (gen in genomelist){
+    dclus[dclus$genome == gen, ]$color <- ave(as.character(dclus[dclus$genome == gen, ]$color),
+                                              dclus[dclus$genome == gen, ]$color, 
+                                              FUN=function(x) if (length(x)>1) paste0(x[1], '_', seq_along(x)) else x[1])
+  }
+  
+  maxR=max(max(table(dclus[,2])),length(unique(dclus[,3])))
+  
+  if(maxR==1){
+    pw=0.4
+  }else if(maxR==2){
+    pw=0.05*maxR/3
+  }else if(maxR==3){
+    pw=0.1*2/3
+  }else{
+    pw=0.09
+  }
+  
   plot1 <- plot1 + geom_fruit(data=dclus,geom=geom_tile,
-               mapping = aes(y=genome, x=color, fill=color),
-               offset=0.05,
-               pwidth=0.02,
-               axis.params=list(
-                 axis="x",
-                 title = cname,
-                 title.size=6,
-                 text.size = 0,
-                 line.size = 0,
-                 vjust = 0
-               ))
+                              mapping = aes(y=genome, x=color, fill=color),pwidth=pw, offset=0.049,
+                              axis.params=list(
+                                axis="x",
+                                title = cname,
+                                title.size=6,
+                                title.angle=-90,
+                                title.height=0.05,
+                                text.size = 0,
+                                line.size = 0,
+                              ))
 }
 
-plot1
+color_values <- c("type0"="green4", "type0_1"="green4", "type0_2"="green",
+                  "type1"="red4", "type1_1"="red4", "type1_2"="red",
+                  "type2"="steelblue", "type2_1"="steelblue", "type2_2"="blue4")
+
+plot1+scale_fill_manual(values=color_values)  + 
+  vexpand(0.04)
+
 # ---------------------------------------------------------------------------------------------------------
 # ---------------------------------------------------------------------------------------------------------
